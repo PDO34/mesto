3f2392c68eb1aa11bece5431b6d44ba6006629fd
Dmitry Pavlov <pavlovdmo@yandex.ru>
2022-03-15 00:24:23 +0300

    ver 5.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,136 @@
-// Сделал через add и remove, зачем в toggle делать проверку, 
-// если мы изначально знаем что он добавляет если нету и удаляет если есть, 
-// т.е. по идеи это может быть Открытие-закрытием и закрытие-открытием.
-// Если поясните буду благодарен, спасибо.
-
-const popup = document.querySelector('.popup');
-const popupName = document.querySelector('.popup__row_data_name');
-const popupStatus = document.querySelector('.popup__row_data_profession');
+//Карточки
+const elementList = document.querySelector('.elements');
+const elementTemplate = document.querySelector('.element-tamplate').content;
+//Профиль
 const profileName = document.querySelector('.profile__name');
 const profileStatus = document.querySelector('.profile__status');
-const popupOpenBtn = document.querySelector('.profile__edit-btn');
+//Попап редактирование профиля
+const popupEditProfile = document.querySelector('#editProfile');
+const popupEditOpenBtnProfile = document.querySelector('.profile__edit-btn');
+const popupEditCloseBtnProfile = popupEditProfile.querySelector('.popup__btn_effect_close');
+const popupEditSubmitBtnForm = popupEditProfile.querySelector('#editForm');
+//Попап картинки при нажатии
+const popupImage = document.querySelector('#image');
+const popupImagePicture = popupImage.querySelector('.popup__image');
+const popupImageText = popupImage.querySelector('.popup__text');
+const popupImageClose = popupImage.querySelector('.popup__btn_effect_close');
+//Попап добавления карточки
+const popupAddElement = document.querySelector('#addElement');
+const popupAddButtonOpenElement = document.querySelector('.profile__add-btn');
+const popupAddButtonCloseElement = popupAddElement.querySelector('.popup__btn_effect_close');
+const popupAddSubmitElement = popupAddElement.querySelector('#addForm');
 
-//Открываем попап
-const openPopupForm = function () {    
-    popup.classList.add('popup_open');
-    popupName.value = profileName.textContent;
-    popupStatus.value = profileStatus.textContent;
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+//Начальные карточки
+initialCards.reverse().forEach(elem => renderElement(createElement(elem.name, elem.link)));
+
+//Функция добавления карточек(element)
+function createElement(name, link) {
+  const element = elementTemplate.cloneNode(true);
+
+  element.querySelector('.element__title').textContent = name
+  element.querySelector('.element__image').src = link
+  element.querySelector('.element__image').alt = `фото ${name}`
+
+  element.querySelector('.element__btn').addEventListener('click', likeClick);
+  element.querySelector('.element__btn-del').addEventListener('click', elementDel);
+  element.querySelector('.element__image').addEventListener('click', () => {
+    openPopupForm(popupImage), takeImagePopup()
+  });
+  return element
 };
 
-//"слушаем"
-popupOpenBtn.addEventListener('click', openPopupForm);
-
-const popupCloseBtn = document.querySelector('.popup__btn_effect_close');
-
-//Закрываем попап
-const closePopupForm = function () {    
-    popup.classList.remove('popup_open');
+//Функция добавление карточек
+function renderElement(element) {
+  elementList.prepend(element)
 };
 
-//"слушаем"
-popupCloseBtn.addEventListener('click', closePopupForm);
-
-const popupSubmitForm = popup.querySelector ('.popup__form');
-
-//Сохраняем форму
-const submitForm = function(evt) {
-    evt.preventDefault();
-    profileName.textContent = popupName.value;
-    profileStatus.textContent = popupStatus.value;
-    closePopupForm();
+//Функция открытие попапа
+function openPopupForm(selector) {
+  selector.classList.add('popup_open')
 };
 
-//"слушаем"
-popupSubmitForm.addEventListener('submit', submitForm);
+//Функция закрытия попапа
+function closePopupForm(selector) {
+  selector.classList.remove('popup_open')
+};
+
+// Функция Лайк
+function likeClick(event) {
+  event.target.classList.toggle('element__btn_action_active');
+};
+
+//Функция Удаления карточки
+function elementDel(event) {
+  event.target.closest('.element').remove();
+};
+
+//Функция очистки полей попапа
+function cleanBoxPopup (selector) {
+  selector.reset();
+}
+
+//Перенос в попап с изображением
+function takeImagePopup() {
+  text = event.target.closest('.element').querySelector('.element__title').textContent
+  popupImagePicture.src = event.target.src
+  popupImagePicture.alt = `фото ${text}`
+  popupImageText.textContent = text
+};
+
+//Функция переноса из профиля в попап
+function transferEditPopup(selector) {
+  selector.profileName.value = profileName.textContent;
+  selector.profileStatus.value = profileStatus.textContent;
+};
+
+//Функция переноса из попапа в профиль
+function transferPopupProfile(selector) {
+  profileName.textContent = selector.popupName.value
+  profileStatus.textContent = selector.profileJob.value
+};
+
+//"Слушаем"
+popupImageClose.addEventListener('click', () => closePopupForm(popupImage));
+
+popupEditOpenBtnProfile.addEventListener('click', () => { openPopupForm(popupEditProfile), transferEditPopup(popupEditSubmitBtnForm) });
+popupEditCloseBtnProfile.addEventListener('click', () => closePopupForm(popupEditProfile));
+popupEditSubmitBtnForm.addEventListener('submit', () => {
+  event.preventDefault();
+  transferPopupProfile(popupEditSubmitBtnForm);
+  closePopupForm(popupEditProfile);
+});
+
+popupAddButtonOpenElement.addEventListener('click', () => { openPopupForm(popupAddElement), cleanBoxPopup(popupAddSubmitElement) });
+popupAddButtonCloseElement.addEventListener('click', () => closePopupForm(popupAddElement));
+popupAddSubmitElement.addEventListener('submit', () => {
+  event.preventDefault();
+  renderElement(createElement(popupAddSubmitElement.placeName.value, popupAddSubmitElement.placeLink.value));
+  closePopupForm(popupAddElement);
+});
