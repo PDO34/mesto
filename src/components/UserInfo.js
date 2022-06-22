b7cdab27ab65526497ec47abaad4cd2b770e5990
Dmitry Pavlov <pavlovdmo@yandex.ru>
2022-06-22 23:39:59 +0300

    PR8 v2

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,20 +1,20 @@
 export default class UserInfo {
-    constructor({profileNameSelector, profileAboutSelector}) {
-      this._profileNameElement = document.querySelector(profileNameSelector);
-      this._profileAboutElement = document.querySelector(profileAboutSelector);
-    }
-  
-    // Возвращает объект с данными пользователя
-    getUserInfo() {
-      return {
-        userName: this._profileNameElement.textContent,
-        userAbout: this._profileAboutElement.textContent
-      }
-    }
-  
-    // Принимает новые данные пользователя, добавляет их на страницу
-    setUserInfo({userName, userAbout}) {
-      this._profileNameElement.textContent = userName;
-      this._profileAboutElement.textContent = userAbout;
+  constructor({profileNameSelector, profileAboutSelector}) {
+    this._profileNameElement = document.querySelector(profileNameSelector);
+    this._profileAboutElement = document.querySelector(profileAboutSelector);
+  }
+
+  // Возвращает объект с данными пользователя
+  getUserInfo() {
+    return {
+      userName: this._profileNameElement.textContent,
+      userAbout: this._profileAboutElement.textContent
     }
   }
+
+  // Принимает новые данные пользователя, добавляет их на страницу
+  setUserInfo({userName, userAbout}) {
+    this._profileNameElement.textContent = userName;
+    this._profileAboutElement.textContent = userAbout;
+  }
+}
